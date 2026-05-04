36eb969dd340abfc4aba1666bb0b03504fc9f5ea
Tyler Kemnitz <kemnitz@gmail.com>
2026-05-04 15:11:58 -0700

    overhauled fish movement system to appear more natural

--- aquarium.py
+++ aquarium.py
@@ -1,4 +1,5 @@
 import random
+import math
 import py5
 from sea_creatures import Fish
 
@@ -11,26 +12,34 @@ def settings():
     py5.size(WIDTH,HEIGHT)
 
 def setup():
-    py5.background(0,0,0)
     set_mono_font()
 
     # create new fish moving and random direction and speed
     for _ in range(5):
+        # My Fish
         fish_list.append(
-            Fish(random.randint(MARGIN, WIDTH),
-                 random.randint(MARGIN, HEIGHT),
-                 random.uniform(1.5, 2.5) * random.choice([-1, 1]),
-                 random.uniform(-0.5, 0.5),
-                 MARGIN)
+            Fish(
+                x=random.randint(MARGIN, WIDTH - MARGIN),
+                y=random.randint(MARGIN, HEIGHT - MARGIN),
+                speed=random.uniform(0.8, 1.2),
+                angle=random.uniform(-math.pi, math.pi),
+                margin=MARGIN
+            )
         )
 
 def draw():
-    py5.background(0,0,0)
+    """
+    Sets background and renders each fish's updated velocity
+    """
+    py5.background(38,37,67)
     for fish in fish_list:
         fish.update()
         fish.display()
 
 def set_mono_font():
+    """
+    Applies global mono font to rendered aquarium artifacts
+    """
     noto_sans_mono = py5.create_font('Noto Sans Mono', 32)
     py5.text_font(noto_sans_mono)
     py5.text_align(py5.LEFT, py5.CENTER)
